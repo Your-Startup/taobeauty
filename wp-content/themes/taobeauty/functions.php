9cb7782010ca0b4cf0e13f82efb5ce62d3b94886
ftp <shubizle11_test@walter.beget.ru>
2022-10-26 19:05:43 +0300

    add header

--- wp-content/themes/taobeauty/functions.php
+++ wp-content/themes/taobeauty/functions.php
@@ -141,6 +141,9 @@ function taobeauty_scripts() {
 	wp_enqueue_style( 'taobeauty-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'taobeauty-style', 'rtl', 'replace' );
 
+	wp_enqueue_style( 'taobeauty-base-style', get_template_directory_uri() . '/assets/css/base.css', array(), _S_VERSION );
+	wp_style_add_data( 'taobeauty-base-style', 'rtl', 'replace' );
+
 	wp_enqueue_script( 'taobeauty-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
