264876b8746910f89dc606223131be8e4cb29e51
Taurta <surta.nr@yandex.ru>
2022-10-27 22:40:27 +0300

    Добавлены список услуг и прайс на главную

--- wp-content/themes/taobeauty/assets/js/home.js
+++ wp-content/themes/taobeauty/assets/js/home.js
@@ -1,71 +1,75 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    class Accordion {
-      constructor(element, state = false) {
-        if(typeof element === 'object') {
-          this.element = element
-        } else if(typeof element === 'string' && element.startsWith('#' || '.')) {
-          this.element = document.querySelector(element);
+  const openAccordion = (accordion) => {
+    const content = accordion.querySelector(".accordion__content");
+    accordion.classList.add("opend");
+    content.style.maxHeight = content.scrollHeight + "px";
+  };
+  
+  const closeAccordion = (accordion) => {
+    const content = accordion.querySelector(".accordion__content");
+    accordion.classList.remove("opend");
+    content.style.maxHeight = null;
+  };
+
+  if(document.querySelector('.main-slider__slider.swiper')) {
+    const mainSlider = new Swiper('.main-slider__slider.swiper', {
+        direction: 'horizontal',
+        loop: true,
+        pagination: {
+          el: '.main-slider__pagination',
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+          },
+        },
+        navigation: {
+          nextEl: '.main-slider__next',
+        },
+        autoplay: {
+          delay: 3000,
+        },
+    });
+  }
+
+  if(document.querySelector('.accordion__block')) {
+    const accordionsServices = document.querySelectorAll(".accordion__block");
+
+    accordionsServices.forEach((accordion) => {
+      const heading = accordion.querySelector(".accordion__heading");
+      const content = accordion.querySelector(".accordion__content");
+    
+      heading.onclick = () => {
+        if (content.style.maxHeight) {
+          closeAccordion(accordion);
         } else {
-          throw new TypeError('Некорректный селектор');
+          accordionsServices.forEach((accordion) => closeAccordion(accordion));
+          openAccordion(accordion);
         }
-        this.heading = this.element.querySelector('.accordion__heading');
-        this.content = this.element.querySelector('.accordion__content');
-        this.state = state;
-        this.addListeners = this.addListeners.bind(this);
-        this.init();
-      }
+      };
+    });
+  }
+
+  if(document.querySelector('.accordion')) {
+    const accordionsPrices = document.querySelectorAll('.accordion');
+
+    accordionsPrices.forEach(accordion => {
+      const heading = accordion.querySelector(".accordion__heading");
+      const content = accordion.querySelector(".accordion__content");
     
-      open() {
-        this.element.classList.add('opend');
-        this.content.style.maxHeight = `${this.content.scrollHeight}px`;
-        this.state = true;
-      }
-    
-      close() {
-        this.element.classList.remove('opend');
-        this.content.style.maxHeight = `0`;
-        this.state = false;
-      }
-    
-      addListeners() {
-        this.heading.addEventListener('click', () => {
-          if(this.state === true) {
-            this.close();
-          } else {
-            this.open();
-          }
-        });
-      }
-    
-      init() {
-        this.addListeners();
-      }
-    };
-  
-    if(document.querySelector('.main-slider__slider.swiper')) {
-      const mainSlider = new Swiper('.main-slider__slider.swiper', {
-          direction: 'horizontal',
-          loop: true,
-          speed: 2000,
-          pagination: {
-            el: '.main-slider__pagination',
-            clickable: true,
-            renderBullet: function (index, className) {
-              return '<span class="' + className + '">' + (index + 1) + "</span>";
-            },
-          },
-          navigation: {
-            nextEl: '.main-slider__next',
-          },
-          autoplay: {
-            delay: 5000,
-          },
-      });
-    }
-  
-    if(document.querySelector('.accordion__block')) {
-      const accList = document.querySelectorAll('.accordion__block');
-      accList.forEach(item => new Accordion(item));
-    }
-  });
+      heading.onclick = () => {
+        if (content.style.maxHeight) {
+          closeAccordion(accordion);
+          accordion.parentElement.classList.remove('opend')
+        } else {
+          accordionsPrices.forEach((accordion) => {
+            closeAccordion(accordion);
+            accordion.parentElement.classList.remove('opend');
+          });
+          openAccordion(accordion);
+          accordion.parentElement.classList.add('opend')
+        }
+      };
+    });
+  }
+});
