0961de479ac4ba0f01a2fd1266b2fb1d24c260e8
Aleksander <aleksander.kononenko2016@yandex.ru>
2022-12-01 11:56:22 +0300

    добавил правки

--- js/components/header.js
+++ js/components/header.js
@@ -3,18 +3,35 @@ export default function initHeader() {
 
     headerItems.forEach((item) => {
 
-        item.addEventListener('mouseenter', () => {
+        item.addEventListener('mouseenter', (e) => {
             headerItems.forEach(hitem => {
                 hitem.classList.remove('active');
             });
             item.classList.add('active');
             document.querySelector('body').style.overflow = 'hidden';
+
+            // if (e.target.classList.contains('fixed-menu__bg')) {
+            //     document.querySelectorAll('header__item').forEach(item => {
+            //         item.classList.remove('active');
+            //         document.querySelector('body').style = null;
+            //     });
+            // }
         });
 
         item.addEventListener('mouseleave', () => {
             item.classList.remove('active');
             document.querySelector('body').style = null;
         });
+    });
 
+    const blocks = document.querySelectorAll('.fixed-menu__block');
+
+    blocks.forEach(block => {
+        block.addEventListener('mouseleave', () => {
+            headerItems.forEach(item => {
+                item.classList.remove('active');
+            });
+            document.querySelector('body').style = null;
+        })
     });
 };
